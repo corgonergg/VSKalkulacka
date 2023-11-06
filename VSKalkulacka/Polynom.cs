580f3d9e005a2515901cc2b9ccdb657bfa23b538
corgonergg <bobesnova@seznam.cz>
2023-11-06 23:08:52 +0100

    1. Základní návrh polynomu

--- VSKalkulacka/Polynom.cs
+++ VSKalkulacka/Polynom.cs
@@ -13,13 +13,39 @@ namespace VSKalkulacka
         private string body;
         private List<int> Coefficients;
 
+        private List<int> numbers = new List<int>(){ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        private string coefficient = "";
+        private bool rad = false;
+        private int poslednirad = 0;
+
         public Polynom(string body)
         {
             this.body = body.Replace(" ", "");
 
             foreach (char c in body)
             {
-                
+                if (char.IsDigit(c))
+                {
+                    if (rad)
+                    {
+                        poslednirad = Convert.ToInt32(c.ToString());
+                        rad = false;
+                    }
+                    else if (!rad && numbers.Contains(Convert.ToInt32(c.ToString())))
+                    {
+                        coefficient += c.ToString();
+                    }
+
+                }
+                else if (c == 'x')
+                {
+                    if (coefficient == "")
+                        Coefficients.Add(1);
+                }
+                else if (c == '^')
+                {
+                    rad = true;
+                }
             }
         }
 
